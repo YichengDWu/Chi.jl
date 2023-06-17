6b24cb510b325cdc71c771e69957d7b919da15b8
Documenter.jl <documenter@juliadocs.github.io>
2023-06-17 19:17:52 +0000

    build based on 813e03c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Chi","category":"page"},{"location":"#Chi","page":"Home","title":"Chi","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Chi.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Chi]","category":"page"}]
+}
